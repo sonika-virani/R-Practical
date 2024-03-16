faf3e54959930efef457e3a502b8086887bff0df
sonika-virani <sonikavirani123@gmail.com>
2024-03-16 13:43:36 +0530

    Dynamically genertes a table

---
+++ src/pages/Search.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+function Search() {
+
+    /*const dataList = [{
+        id: 1, name: 'foo', city: 'dallas', category: 'one', type: 'A', active: 'FALSE'
+    }, {
+        id: 2, name: 'bar', city: 'dallas', category: 'one', type: 'B', active: 'FALSE'
+    }, {
+        id: 3, name: 'jim', city: 'san francisco', category: 'one', type: 'B', active: 'TRUE',
+    }, {
+        id: 4, name: 'jane', city: 'denver', category: 'two', type: 'C', active: 'FALSE'
+    }]*/
+
+    const dataList = [
+        {
+            id: 1,
+            mall: 'V R mall',
+            address: 'Surat',
+            rating: 'A'
+        },
+        {
+            id: 2,
+            mall: 'Rahul Raj Mall',
+            address: 'dallas',
+            rating: 'B'
+        },
+        {
+            id: 3,
+            mall: 'Raj Imperial',
+            address: 'san francisco',
+            category: 'one',
+            rating: 'B'
+        },
+        {
+            id: 4,
+            mall: 'jane',
+            address: 'denver',
+            category: 'two',
+            rating: 'C'
+        }
+    ]
+
+
+    return (
+        <div>
+            <div className="col-12">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        {Object.keys(dataList.reduce((acc, obj) => ({...acc, ...obj}), {})).map((key, index) => (
+                            <th key={index} scope="col">{key.charAt(0).toUpperCase() + key.slice(1)}</th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {dataList.map((item, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {Object.keys(dataList.reduce((acc, obj) => ({...acc, ...obj}), {})).map((key, colIndex) => (
+                                <td key={colIndex}>{item[key] || '-'}</td>
+                            ))}
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
